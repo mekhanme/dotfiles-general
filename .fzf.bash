009dff7147e7ccce21e292e56a1ec07cf9eaf305
mikh <mikh@local>
2022-05-04 19:22:53 +0300

    change fzf config

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */tmp/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/tmp/fzf/bin"
+  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/tmp/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/tmp/fzf/shell/key-bindings.bash"
+source "~/.fzf/shell/key-bindings.bash"
